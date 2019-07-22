e002f9df59f649eb8edabc8b0455e539b7cd3ff1
alexander lerch <alexander.lerch@gatech.edu>
2019-07-22 16:19:18 +0800

    minor updates to AMDF freq tracking

--- PitchTimeAmdf.m
+++ PitchTimeAmdf.m
@@ -29,14 +29,19 @@ function [f, t] = PitchTimeAmdf(x, iBlockLength, iHopLength, f_s)
     for (n = 1:iNumOfBlocks)
         i_start         = (n-1)*iHopLength + 1;
         i_stop          = min(length(x),i_start + iBlockLength - 1);
+  
+        if (sum(abs(x(i_start:i_stop))) == 0)
+            f(n) = 0;
+            continue;
+        end
         
         % calculate the amdf minimum
         afAMDF          = amdf(x(i_start:i_stop), eta_max);
         [fDummy, f(n)]  = min(afAMDF(1+eta_min:end));
+        
+        % convert to Hz
+        f(n) = f_s ./ (f(n) + eta_min);
     end
- 
-    % convert to Hz
-    f                   = f_s ./ (f + eta_min);
 end
 
 function [AMDF] = amdf(x, eta_max)
@@ -44,7 +49,7 @@ function [AMDF] = amdf(x, eta_max)
  
     AMDF    = ones(1, K);
     
-    for (eta=0:min(K-1,eta_max))
-        AMDF(eta+1) = sum(abs(x(1:K-eta)-x(eta+1:end)))/K;
+    for (eta=0:min(K-1,eta_max-1))
+        AMDF(eta+1) = sum(abs(x(1:K-1-eta)-x(eta+2:end)))/K;
     end
 end
