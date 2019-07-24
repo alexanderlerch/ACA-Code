4adafddf52800d00ea0f56b7187141f10eafc0ed
alexander lerch <alexander.lerch@gatech.edu>
2019-07-24 17:06:58 +0800

    - updated max search - cleanup

--- PitchSpectralAcf.m
+++ PitchSpectralAcf.m
@@ -5,15 +5,14 @@
 %> @param X: spectrogram (dimension FFTLength X Observations)
 %> @param f_s: sample rate of audio data 
 %>
-%> @retval f acf maximum (in Hz)
+%> @retval f acf maximum location (in Hz)
 % ======================================================================
 function [f] = PitchSpectralAcf (X, f_s)
 
     % initialize
-    iOrder  = 4;
     f_min   = 300;
     
-    eta_min = round(f_min/f_s * 2 * size(X,1));
+    
 
     % allocate
     f       = zeros(1, size(X,2));
@@ -24,11 +23,17 @@ function [f] = PitchSpectralAcf (X, f_s)
     
     % compute the ACF
     for (n = 1: size(X,2))
-        afCorr          = xcorr(X(:,n),'coeff');
-        afCorr          = afCorr((ceil((length(afCorr)/2))+1):end);
-        [fDummy, f(n)]  = max(afCorr(1+eta_min:end));
+        eta_min = round(f_min/f_s * (size(X,1)-2));
+        afCorr  = xcorr(X(:,n),'coeff');
+        afCorr  = afCorr((ceil((length(afCorr)/2))+1):end);
+        
+        % find local maxima
+        [fDummy,eta_peak]   = findpeaks(afCorr);
+
+        eta_min = max(eta_min, find(eta_peak > eta_min,1));
+        [fDummy, f(n)]  = max(afCorr(eta_min:end));
     end
     
     % find max index and convert to Hz (note: X has double length)
-    f           = (f + eta_min - 1) / size(X,1) * f_s;
+    f           = (f + eta_min - 1) / (size(X,1)-2) * f_s;
 end
