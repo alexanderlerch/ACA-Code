df48564e24a4a82f1a6432c4c35031b3b39dc87a
alexander lerch <alexander.lerch@gatech.edu>
2020-08-09 11:27:04 -0400

    flipped output dimensions to be consistent with other features

--- FeatureSpectralSkewness.m
+++ FeatureSpectralSkewness.m
@@ -27,7 +27,7 @@ function [vssk] = FeatureSpectralSkewness (X, f_s)
         std_X   = FeatureSpectralSpread(X, f_s);
         tmp     = repmat(f, size(X,2),1) - repmat(mu_X, size(X,1),1)';
         
-        vssk    = sum((tmp.^3)'.*X)' ./ (std_X'.^3 .* sum(X,1)'*size(X,1));
+        vssk    = sum((tmp.^3)'.*X)' ./ (std_X'.^3 .* sum(X,1)'*size(X,1))';
     end
        
     % avoid NaN for silence frames
