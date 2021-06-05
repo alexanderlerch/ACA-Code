b1538a663080513fe422d7d09145b78149b5cb8f
alexander lerch <alexander.lerch@gatech.edu>
2021-06-05 12:31:35 -0400

    - added single pole RMS estimation

--- FeatureTimeRms.m
+++ FeatureTimeRms.m
@@ -12,6 +12,9 @@
 % ======================================================================
 function [vrms, t] = FeatureTimeRms(x, iBlockLength, iHopLength, f_s)
 
+    T_i         = .3; 
+    alpha       = 1-exp(-2.2/f_s/T_i);
+
     % number of results
     iNumOfBlocks    = floor ((length(x)-iBlockLength)/iHopLength + 1);
     
@@ -19,16 +22,20 @@ function [vrms, t] = FeatureTimeRms(x, iBlockLength, iHopLength, f_s)
     t               = ((0:iNumOfBlocks-1) * iHopLength + (iBlockLength/2))/f_s;
     
     % allocate memory
-    vrms            = zeros(1,iNumOfBlocks);
+    vrms            = zeros(2,iNumOfBlocks);
 
+    % single pole implementation
+    v_sp            = filter(alpha, [1 -(1-alpha)],x.^2);
+    
     for (n = 1:iNumOfBlocks)
         i_start     = (n-1)*iHopLength + 1;
         i_stop      = min(length(x),i_start + iBlockLength - 1);
         
         % calculate the rms
-        vrms(n)     = sqrt(mean(x(i_start:i_stop).^2));
+        vrms(1,n)   = sqrt(mean(x(i_start:i_stop).^2));
+        vrms(2,n)   = max(sqrt(v_sp(i_start:i_stop)));
     end
- 
+
     % convert to dB
     epsilon         = 1e-5; %-100dB
     
