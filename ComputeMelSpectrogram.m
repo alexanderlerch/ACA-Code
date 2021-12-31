2a969d94edbaf88c1d7093b738ded11291fbaa57
alexanderlerch <alexander.lerch@gatech.edu>
2021-12-31 17:07:39 -0500

    fixed comment and renamed some variables

--- ComputeMelSpectrogram.m
+++ ComputeMelSpectrogram.m
@@ -1,7 +1,7 @@
 % ======================================================================
 %> @brief computes a mel spectrogram from the audio data
 %>
-%> @param afAudioData: time domain sample data, dimension channels X samples
+%> @param x: time domain sample data, dimension channels X samples
 %> @param f_s: sample rate of audio data
 %> @param bLogarithmic: levels (true) or magnitudes (false)
 %> @param afWindow: FFT window of length iBlockLength (default: hann), can be [] empty
@@ -10,10 +10,11 @@
 %> @param iNumMelBands: num of frequency bands (default: 128)
 %> @param fMax: maximum frequency (default: f_s/2)
 %>
-%> @retval v feature value
-%> @retval t time stamp for the feature value
+%> @retval M spectrogram
+%> @retval f frequency bands
+%> @retval t time stamps
 % ======================================================================
-function [M, f_c, t] = ComputeMelSpectrogram (afAudioData, f_s, bLogarithmic, afWindow, iBlockLength, iHopLength, iNumMelBands, fMax)
+function [M, f_c, t] = ComputeMelSpectrogram (x, f_s, bLogarithmic, afWindow, iBlockLength, iHopLength, iNumMelBands, fMax)
 
     % set default parameters if necessary
     if (nargin < 8)
@@ -36,7 +37,7 @@ function [M, f_c, t] = ComputeMelSpectrogram (afAudioData, f_s, bLogarithmic, af
     end
     if (nargin < 1)
         load handel;
-        afAudioData     = y;
+        x     = y;
         f_s             = Fs;
         clear y,Fs;
     end
@@ -46,15 +47,15 @@ function [M, f_c, t] = ComputeMelSpectrogram (afAudioData, f_s, bLogarithmic, af
     end
     
     % pre-processing: down-mixing
-    afAudioData = ToolDownmix(afAudioData);
+    x = ToolDownmix(x);
     
     % pre-processing: normalization 
-    afAudioData = ToolNormalizeAudio(afAudioData);
+    x = ToolNormalizeAudio(x);
  
-    afAudioData = [afAudioData; zeros(iBlockLength,1)];
+    x = [x; zeros(iBlockLength,1)];
     
     % in the real world, we would do this block by block...
-    [X,f,t]     = spectrogram(  afAudioData,...
+    [X,f,t]     = spectrogram(  x,...
                                 afWindow,...
                                 iBlockLength-iHopLength,...
                                 iBlockLength,...
