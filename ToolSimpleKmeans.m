eee8760d415d8590a0c69bac5fccc870e0c054dc
alexanderlerch <alexander.lerch@gatech.edu>
2022-01-07 20:16:27 -0500

    some fixes in kmeans implementation (mostly init)

--- ToolSimpleKmeans.m
+++ ToolSimpleKmeans.m
@@ -1,7 +1,7 @@
 % ======================================================================
 %> @brief kmeans clustering
 %>
-%> @param FeatureMatrix: features for all train observations (dimension iNumFeatures x iNumObservations)
+%> @param V: features for all train observations (dimension iNumFeatures x iNumObservations)
 %> @param k: number of clusters
 %> @param numMaxIter: maximum number of iterations (stop if not converged before)
 %> @param prevState: internal state that can be stored to continue clustering later
@@ -9,7 +9,7 @@
 %> @retval clusterIdx cluster index of each observation
 %> @retval state internal state (only if needed)
 % ======================================================================
-function [clusterIdx,state] = ToolSimpleKmeans(FeatureMatrix, k, numMaxIter, prevState)
+function [clusterIdx,state] = ToolSimpleKmeans(V, K, numMaxIter, prevState)
     
     if (nargin < 3)
         numMaxIter  = 1000;
@@ -19,28 +19,27 @@ function [clusterIdx,state] = ToolSimpleKmeans(FeatureMatrix, k, numMaxIter, pre
     else
         % initialize
         % use fixed seed for reproducibility (comment out if needed)
-        rng(42); 
+        %rng(42); 
         
         % pick random observations as cluster means
-        state = computeClusterMeans_I(FeatureMatrix,...
-            round(rand(1,size(FeatureMatrix,2))*(k-1))+1, k);
+        state = struct('m',V(:, round(rand(1,K)*(size(V,2)-1))+1)); 
     end
-    range = [min(FeatureMatrix,[],2) max(FeatureMatrix,[],2)];
+    range_V = [min(V,[],2) max(V,[],2)];
     
     % assign observations to clusters
-    clusterIdx = assignClusterLabels_I(FeatureMatrix,state);
+    clusterIdx = assignClusterLabels_I(V,state);
     
     for i=1:numMaxIter
         prevState = state;
         
         % update means
-        state = computeClusterMeans_I(FeatureMatrix,clusterIdx,k);
+        state = computeClusterMeans_I(V,clusterIdx,K);
         
         % reinitialize empty clusters
-        state = reinitState_I(state, clusterIdx, k, range);
+        state = reinitState_I(state, clusterIdx, K, range_V);
         
         % assign observations to clusters
-        clusterIdx = assignClusterLabels_I(FeatureMatrix,state);
+        clusterIdx = assignClusterLabels_I(V,state);
         
         % if we have converged, break
         if (max(sum(abs(state.m-prevState.m)))==0)
@@ -49,21 +48,24 @@ function [clusterIdx,state] = ToolSimpleKmeans(FeatureMatrix, k, numMaxIter, pre
     end
 end
 
-function [clusterIdx]  = assignClusterLabels_I(FeatureMatrix,state)
+function [clusterIdx]  = assignClusterLabels_I(V,state)
+
+    K = size(state.m, 2);
+    
     % compute distance to all points
-    for (k=1:length(state.m))
-        D(k,:) = sqrt(sum((repmat(state.m(:,k),1,size(FeatureMatrix,2))-FeatureMatrix).^2,1));
+    for (k=1:K)
+        D(k,:) = sqrt(sum((repmat(state.m(:,k),1,size(V,2))-V).^2,1));
     end
     
     % assign to closest
     [dummy,clusterIdx] = min(D,[],1);
 end
 
-function [state] = computeClusterMeans_I(FeatureMatrix,clusterIdx,K)
-    m = zeros(size(FeatureMatrix,1), K);
+function [state] = computeClusterMeans_I(V,clusterIdx,K)
+    m = zeros(size(V,1), K);
     for (k=1:K)
         if~(isempty(find(clusterIdx==k)))
-            m(:,k) = mean(FeatureMatrix(:,find(clusterIdx==k)),2);
+            m(:,k) = mean(V(:,find(clusterIdx==k)),2);
         end
     end
     state = struct('m',m);
