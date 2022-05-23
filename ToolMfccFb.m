cc2f90816036bc9800af6a3895e04f60222759ba
alexanderlerch <alexander.lerch@gatech.edu>
2022-05-23 15:47:45 -0400

    reformatting

--- ToolMfccFb.m
+++ ToolMfccFb.m
@@ -1,5 +1,5 @@
 %> see function mfcc.m from Slaneys Auditory Toolbox
-function [H] = ToolMfccFb (iFftLength, f_s)
+function [H] = ToolMfccFb (iMagSpecLength, f_s)
 
     % initialization
     f_start         = 133.3333;
@@ -12,25 +12,25 @@ function [H] = ToolMfccFb (iFftLength, f_s)
     logSpacing      = 1.0711703;
 
     % compute band frequencies
-    f               = f_start + (0:iNumLinFilters-1)*linearSpacing;
+    f = f_start + (0:iNumLinFilters-1)*linearSpacing;
     f(iNumLinFilters+1:iNumFilters+2) = ...
 		      f(iNumLinFilters) * logSpacing.^(1:iNumLogFilters+2);
 
     % sanity check
-    f               = f (f<f_s/2);
-    iNumFilters     = length(f)-2;
+    f = f (f<f_s/2);
+    iNumFilters = length(f)-2;
     
-    f_l             = f(1:iNumFilters);
-    f_c             = f(2:iNumFilters+1);
-    f_u             = f(3:iNumFilters+2);
+    f_l = f(1:iNumFilters);
+    f_c = f(2:iNumFilters+1);
+    f_u = f(3:iNumFilters+2);
 
     % allocate memory for filters and set max amplitude
-    H               = zeros(iNumFilters,iFftLength);
-    afFilterMax     = 2./(f_u-f_l);
-    f_k             = (0:iFftLength-1)/(iFftLength-1)*f_s/2;
+    H = zeros(iNumFilters,iMagSpecLength);
+    afFilterMax = 2./(f_u-f_l);
+    f_k = (0:iMagSpecLength-1)/(iMagSpecLength-1)*f_s/2;
 
     % compute the transfer functions
-    for (c = 1:iNumFilters)
+    for c = 1:iNumFilters
         H(c,:)      = ...
             (f_k > f_l(c) & f_k <= f_c(c)).* ...
             afFilterMax(c).*(f_k-f_l(c))/(f_c(c)-f_l(c)) + ...
